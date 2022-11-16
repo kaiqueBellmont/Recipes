54897917cd6aaff42649c4e5b764c1fafb8722d9
KaiqueBellmont <kaiquebellmont@gmail.com>
2022-11-16 18:36:47 -0300

    quase finalizando os testes

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
@@ -28,10 +29,21 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True, )
 
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
     })
 
+
+def search(request):
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        raise Http404()
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+    })
